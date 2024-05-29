abeb2dabc420ecde8040f2d9d47595cf4094d867
EasySimho <s.benanchietti.yt@gmail.com>
2024-05-29 22:17:08 +0200

    hi

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ require('dotenv').config({ path: '.env.vercel' });;
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
